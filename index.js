05b3c2b20de9afb158adfaaffb18774979683029
Oscar Sanabria <oscarsanabria28@gmail.com>
2017-11-11 08:35:43 -0600

    Setup server

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+
+app.get('/', (req,res)=>{
+	Logger.debug("Oscar");
+	res.send('<h1 align="center">TransferMed</h1>');
+});
+
+var server= app.listen(process.env.PORT || 3000, ()=>{
+	console.log('New local server for Bragi Tools is ready at port 3000');
+});
