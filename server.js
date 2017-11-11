2e00dff7e0289de0d92f408bf7828e920e28b949
BryanMG125 <bryan_mg125@hotmail.com>
2017-11-11 09:34:48 -0600

    pagina con css y todo

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require("express");
 var app = express();
 
+const path              = require('path');
+
+app.use(express.static(path.join(__dirname,'pages')));
+
+
 app.get('/', (req,res)=>{
-	res.send('<h1 align="center">TransferMed</h1>');
+	res.sendFile( path.join( __dirname , "index.html" ) );
 });
 
 var server= app.listen(process.env.PORT || 3000, ()=>{
